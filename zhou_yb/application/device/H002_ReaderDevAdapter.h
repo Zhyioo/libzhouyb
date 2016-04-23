05957798fa779805c928dbc1cc9ff16b353d0190
Zhyioo <zhyioo@vip.qq.com>
2016-04-24 00:21:03 +0800

    1.H002_ReaderDevAdapter.h中增加轮询等待磁条的接口WaitForMagnetic

--- zhou_yb/application/device/H002_ReaderDevAdapter.h
+++ zhou_yb/application/device/H002_ReaderDevAdapter.h
@@ -11,6 +11,8 @@
 #define _LIBZHOUYB_H002_READERDEVADAPTER_H_
 //--------------------------------------------------------- 
 #include "../../include/Base.h"
+#include "../../device/magnetic/MagneticDevAdapter.h"
+using zhou_yb::device::magnetic::MagneticDevAdapter;
 //--------------------------------------------------------- 
 namespace zhou_yb {
 namespace application {
@@ -69,6 +71,38 @@ public:
         DevCommand::FromAscii("1B 50", cmd);
         cmd += ledMode;
         ASSERT_FuncErr(_pDev->Write(cmd), DeviceError::SendErr);
+
+        return _logRetValue(true);
+    }
+    /**
+     * @brief 等待磁条数据
+     * @date 2016-04-23 23:08
+     * 
+     * @param [out] hasMag 是否有磁条数据
+     * @param [out] tr1 获取到的一磁道信息
+     * @param [out] tr2 获取到的二磁道信息
+     * @param [out] tr3 获取到的三磁道信息
+     */
+    bool WaitForMagnetic(bool& hasMag, ByteBuilder* tr1 = NULL, ByteBuilder* tr2 = NULL, ByteBuilder* tr3 = NULL)
+    {
+        LOG_FUNC_NAME();
+        ASSERT_Device();
+
+        ByteBuilder cmd(4);
+        DevCommand::FromAscii("1B 50", cmd);
+        ASSERT_FuncErr(_pDev->Write(cmd), DeviceError::SendErr);
+        ByteBuilder recv(64);
+        while(recv.GetLength() < 2)
+        {
+            ASSERT_FuncErr(_pDev->Read(recv), DeviceError::RecvErr);
+        }
+        ASSERT_FuncErr(recv[0] == 0x1B, DeviceError::RecvFormatErr);
+        hasMag = (recv[1] == '1');
+        if(!hasMag)
+            return _logRetValue(true);
+        // 继续接收磁条数据
+        ASSERT_FuncErr(MagneticDevAdapter::RecvByFormat(_pDev, recv), DeviceError::RecvErr);
+        MagneticDevAdapter::UnpackRecvCmd(recv.SubArray(2), tr1, tr2, tr3);
 
         return _logRetValue(true);
     }
